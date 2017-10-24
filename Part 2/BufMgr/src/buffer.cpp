24150ffab3cb222008e0fb6e209d41ccc372ddb3
mtlang <mtlang@comcast.net>
2017-10-24 14:47:17 -0500

    Moved Nate's changes to src

--- Part 2/BufMgr/src/buffer.cpp
+++ Part 2/BufMgr/src/buffer.cpp
@@ -39,26 +39,69 @@ BufMgr::~BufMgr() {
 	// Flush dirty pages
 	for (int i = 0; i < numBufs; i++)
 	{
-		if (bufDescTable[i].dirty) 
+		if (bufDescTable[i].dirty)
 		{
 			File->writePage(&bufPool[i]);
 			bufDescTable[i].dirty = false;
 		}
 	}
-	
+
 	// Deallocate bufPool and bufDescTable
 	delete bufPool;
 	delete bufDescTable;
-	
 }
 
+//Increments clockhand clockwise. If clockhand points to the last page in the pool, set to zero
 void BufMgr::advanceClock()
 {
-
+	if (clockHand < numBufs - 1) {
+		clockHand++;
+	}
+	else {
+		clockHand = 0;
+	}
 }
-
+//evicts page in buffer pool using clock replacement policy
+//clears frame at FrameID passed to allocBuf
 void BufMgr::allocBuf(FrameId & frame) 
 {
+		advanceClock();
+		bool validSet = bufDescTable[clockHand].valid;
+		bool refBitSet = bufDescTable[clockHand].refbit;
+		bool pinned = bufDescTable[clockHand].pinCnt > 0;
+		bool dirty = bufDescTable[clockHand].dirty;
+		FrameId clockStart = clockHand;
+
+		//check if all frames are pinned
+		while (bufDescTable[clockHand].pinCnt > 0) {
+			advanceClock();
+			//loop through the buffer pool until an unpinned page is found
+			//if the hand makes it all around they're all pinned
+			if (clockHand == clockStart)
+				throw new BufferExceededException;
+		}
+
+		//reset clockHand to where it started and begin searching for a page to evict
+		clockHand = clockStart;
+		while (validSet && (refBitSet || pinned)) {
+			if (refBitSet)
+				bufDescTable[clockHand].refbit = false;
+			advanceClock();
+			validSet = bufDescTable[clockHand].valid;
+			refBitSet = bufDescTable[clockHand].refbit;
+			pinned = bufDescTable[clockHand].pinCnt > 0;
+			dirty = bufDescTable[clockHand].dirty;
+
+		}
+		if (dirty && validSet)
+			flushFile(bufDescTable[clockHand].file);
+
+		//once victim page is found, the page at the requested FrameID (frame) is moved to the victim pages slot
+		//the requested frame is then cleared 
+		bufDescTable[clockHand].Set(bufDescTable[frame].file, bufDescTable[frame].pageNo);
+		bufDescTable[frame].Clear();
+
+
 }
 
 	
@@ -69,40 +112,66 @@ void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+	//check if page exists in bufferpool
+	FrameId clockStart = clockHand;
+	FrameId frame;
+	bool notFound;
+	while (bufDescTable[clockHand].pageNo != pageNo) {
+		advanceClock();
+		//loop through the buffer pool until desired page is found
+		//if the hand makes it all around it's not in the buffer pool
+		if (clockHand == clockStart)
+			notFound = true;
+	}
+
+	if (!notFound) {
+		if (bufDescTable[clockHand].pinCnt == 0) {
+			throw new PageNotPinnedException("Page not pinned", pageNo, clockHand);
+		}
+
+		else {
+			if (dirty) {
+				bufDescTable[clockHand].dirty = true;
+			}
+			bufDescTable[clockHand].pinCnt--;
+		}
+
+	}
+	clockHand = clockStart;
 }
 
 void BufMgr::flushFile(const File* file) 
 {
 }
 
-void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
+void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page)
 {
 	// Allocate an empty page
 	Page newPage = file->allocatePage();
-	
+
 	// Obtain a buffer pool frame
 	FrameId bufFrame;
 	allocBuf(&bufFrame);
-	
+
 	// Insert entry into hash table
-	hashTable.insert(file, *pageNo, bufFrame);
-	
+	hashTable->insert(file, *pageNo, bufFrame);
+
 	// Set up frame
 	bufDescTable[bufFrame].Set(file, *pageNo);
-	
+
 }
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
 {
 	Page * toDispose = file->readPage(PageNo, false);
-    // If page is in buffer pool...
+	// If page is in buffer pool...
 	Page * inPool = std::find(std::begin(bufPool), std::end(bufPool), *toDispose);
-	if (inPool != std::end(bufPool)) 
+	if (inPool != std::end(bufPool))
 	{
 		// Free page from pool
 		*inPool = null;
 		// Remove page from hash table
-		hashTable.remove(file,PageNo);
+		hashTable.remove(file, PageNo);
 	}
 	// Delete page from file
 	file->deletePage(PageNo);
