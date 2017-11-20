eb14e39140da4c7ddd8698a7586c059aba7ea376
kbstoner <kstoner246@gmail.com>
2017-11-20 11:58:35 -0600

    insertEntry and startScan progress

--- Part 3/Btree/src/btree.cpp
+++ Part 3/Btree/src/btree.cpp
@@ -95,6 +95,101 @@ BTreeIndex::~BTreeIndex()
 
 const void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
 {
+    //read in the root page; try to cast it to a leaf/non-leaf node
+    NonLeafNodeInt* node = &(file->readPage(rootPageNum));   
+    LeafNodeInt* leaf = null;
+    bool found;
+    int i, leafLength, insertAt = -1;   
+    
+    //start at root node and traverse to the correct leaf node
+    while(leaf == null) {
+        found = false;
+    
+        //find the correct range for the parameter key
+        //when found, always check the level to see if the next page is a nonleaf or leaf node
+        for (i = 0; i < INTARRAYNONLEAFSIZE && node.pageNoArray[i] != null && !found; i++) {
+            
+            //check if key is less than the smallest value
+            if (i == 0) {
+                if (*key < node.keyArray[0]) {
+                    found = true;
+                    if (node.level == 1) {
+                        leaf = &(file->readPage(node.pageNoArray[0]));
+                    }
+                    else {
+                        node = &(file->readPage(node.pageNoArray[0]));
+                    }                    
+                }
+            }
+            //the key must be larger than the largest value
+            else if (i == INTARRAYNONLEAFSIZE - 1) {
+                found = true;
+                if (node.level == 1) {
+                    leaf = &(file->readPage(node.pageNoArray[i + 1]));
+                }
+                else {
+                    node = &(file->readPage(node.pageNoArray[i + 1]));
+                }
+            }
+            else {
+                //check that the key is inbetween the two given values
+                if (*key >= node.keyArray[i] && *key < node.keyArray[i + 1]) {
+                    found = true;
+                    if (node.level == 1) {
+                        leaf = &(file->readPage(node.pageNoArray[i + 1]));
+                    }
+                    else {
+                        node = &(file->readPage(node.pageNoArray[i + 1]));
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    //determine how many values are in the leaf
+    for (i = 0; i < INTARRAYLEAFSIZE && leaf.keyArray[i] != null) {
+        //the leaf is full
+        if (i == INTARRAYLEAFSIZE - 1) {
+            leafLength = INTARRAYLEAFSIZE;
+        }
+        //check if the next value is null
+        else if (leaf.keyArray[i + 1] == null) {
+            leafLength = i + 1;
+        }
+    }
+    
+    //find the spot in the leaf node to insert into
+    for (i = 0; i < leafLength && insertAt == -1; i++) {
+        if (i == 0) {
+            if (*key < leaf.keyArray[0]) {
+                insertAt = i;
+            }
+        }
+        else if (i == leafLength - 1) {
+            if (*key > leaf.keyArray[leafLength - 1]) {
+                insertAt = leafLength;
+            }
+        }
+        else if (*key >= leaf.keyArray[i] && *key < leaf.keyArray[i + 1]) {
+            insertAt = i + 1;
+        }
+    }
+    
+    //if the leaf is full, split
+    if (leafLength == INTARRAYLEAFSIZE) {
+        
+    }
+    //leaf is not full; insert at the correct index
+    //must shift previous array entries
+    else {       
+        for (i = leafLength; i > insertAt; i--) {
+            leaf.keyArray[i] = leaf.keyArray[i - 1];
+            leaf.ridArray[i] = leaf.ridArray[i - 1];
+        }
+        leaf.keyArray[insertAt] = *key;
+        leaf.ridArray[i] = rid;
+    }
 
 }
 
@@ -107,9 +202,35 @@ const void BTreeIndex::startScan(const void* lowValParm,
 				   const void* highValParm,
 				   const Operator highOpParm)
 {
-
+    //check for in-progress scan
+    if (scanExecuting) {
+        endScan();
+    }
+    
+    //check for exceptions
+    if (*lowValParm > *highValParm) {
+        throw BadScanrangeException();
+    }
+    if (lowOpParm != "GT" && lowOpParm != "GTE") {
+        throw BadOpcodesException();
+    }
+    if (highOpParm != "LT" && highOpParm != "LTE") {
+        throw BadOpcodesException();
+    }
+    
+    //set scanning params
+    scanExecuting = true;
+    lowOp = lowOpParm;
+    highOp = highOpParm;
+    
+    //check the type of the low/high parms
+    
+    
+    //starting at root, traverse index tree; throw exception if no matches found
+    
+    
 }
-
+	
 // -----------------------------------------------------------------------------
 // BTreeIndex::scanNext
 // -----------------------------------------------------------------------------
