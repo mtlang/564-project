dc1e1b68d1f5dd42e154746b65918520c5d1a355
kbstoner <kstoner246@gmail.com>
2017-12-09 10:05:56 -0600

    Update runParser.sh

--- Part 4/auctionbase/create_auctionbase/runParser.sh
+++ Part 4/auctionbase/create_auctionbase/runParser.sh
@@ -4,11 +4,4 @@ python skeleton_parser.py /usr/class/cs145/project/ebay_data/items-*.json
 sort -u items.dat -o itemsUnique.dat
 sort -u categories.dat -o categoriesUnique.dat
 sort -u bids.dat -o bidsUnique.dat
-=======
-python skeleton_parser.py C:/Users/Kaitlin/Documents/Kaitlin/Programming/CS_564/P4/auctionbase/ebay_data/items-*.json
-
-sort -u items.dat -o itemsUnique.dat
-sort -u categories.dat -o categoriesUnique.dat
-sort -u bids.dat -o bidsUnique.dat
->>>>>>> e698297ba16590c25c481c8d843ffdadcb51fcad
 sort -u users.dat -o usersUnique.dat
