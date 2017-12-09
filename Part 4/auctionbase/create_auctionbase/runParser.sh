dea499bbdd37c4ee7aa95f1e8447a9d3afcbffcb
kbstoner <kstoner246@gmail.com>
2017-12-09 09:57:19 -0600

    Add files via upload

---
+++ Part 4/auctionbase/create_auctionbase/runParser.sh
@@ -0,0 +1,6 @@
+python skeleton_parser.py C:/Users/Kaitlin/Documents/Kaitlin/Programming/CS_564/P4/auctionbase/ebay_data/items-*.json
+
+sort -u items.dat -o itemsUnique.dat
+sort -u categories.dat -o categoriesUnique.dat
+sort -u bids.dat -o bidsUnique.dat
+sort -u users.dat -o usersUnique.dat
