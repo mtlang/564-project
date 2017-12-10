4f1b634d79941414ce188fde31d01a4588852445
mtlang <mtlang@comcast.net>
2017-12-10 13:09:58 -0600

    Transferred nate's helper methods in sqlitedb.py

--- Part 4/auctionbase/web.py/sqlitedb.py
+++ Part 4/auctionbase/web.py/sqlitedb.py
@@ -1,9 +1,10 @@
 import web
 
 db = web.database(dbn='sqlite',
-        db='AuctionBase' #TODO: add your SQLite database filename
-    )
+				  db='AuctionBase' #TODO: add your SQLite database filename
+				  )
 
+				  
 ######################BEGIN HELPER METHODS######################
 
 # Enforce foreign key constraints
@@ -50,10 +51,13 @@ def setTime(time):
     else:
         t.commit() 
         return True
+	# close all auctions now that time elapsed
+    close_auctions_time();
+    close_auctions_price();
 
 # returns a single item specified by the Item's ID in the database
 # Note: if the `result' list is empty (i.e. there are no items for a
-# a given ID), this will throw an Exception!
+# a given ID), this will return None!
 def getItemById(item_id):
 	query_string = 'select * from Items where item_ID = $itemID'
     try:
@@ -71,3 +75,55 @@ def query(query_string, vars = {}):
 
 #TODO: additional methods to interact with your database,
 # e.g. to update the current time
+
+def close_auctions_time():
+
+    query_string = 'select itemID from Items where Ends < $curr_time'
+    list = query(query_string, {'curr_time': getTime()});
+    for item in list:
+        t = db.transaction()
+        try:
+            db.query('delete from Items where itemID = $item', {'item': item});
+        except Exception as e:
+            t.rollback()
+            print str(e)
+            return False
+        else:
+            t.commit()
+            return True
+
+
+
+def close_auctions_price():
+    query_string = 'select itemID from Items where Currently > Buy_Price or Currnetly = Buy_Price'
+    list = query(query_string);
+    for item in list:
+        t = db.transaction()
+        try:
+            db.query('delete from Items where itemID = $item', {'item': item});
+        except Exception as e:
+            t.rollback()
+            print str(e)
+            return False
+        else:
+            t.commit()
+            return True
+
+
+def get_item_attributes(ItemID):
+    return db.query('select * from Items where ItemID = $ItemID', {'ItemID' : ItemID})
+
+def get_item_categories(ItemID):
+    return db.query('select Categories from Items where ItemID = $ItemID', {'ItemID': ItemID});
+
+def get_item_status(ItemID):
+    list = db.query('select Categories from Items where ItemID = $ItemID', {'ItemID': ItemID});
+    if (len(list) == 0): return 'closed';
+    else: return 'open';
+
+def get_auction_bids(ItemID):
+    if (get_item_status(ItemID) == 'open'):
+        return db.query('select UserID, Time, Amount from Bids where ItemID = $ItemID', {'ItemID': ItemID});
+    else:
+        return db.query('select UserID from Bids where Amount = (select MAX(Amount) from Bids)')
+
